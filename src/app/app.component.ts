74f972e0769de0e953a5710ecf00972e6869d6e3
gileadekelvin <gileadekelvin@gmail.com>
2018-07-16 15:54:37 -0300

    Modifica nome da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Capital dos Políticos';
+  title = 'Capital dos Candidatos';
 
   constructor(private router: Router) { }
 
